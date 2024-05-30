f042670d22c8c2c17831a81aaf6cd602f1772262
Hisun0 <tridvad1@gmail.com>
2024-05-30 12:44:56 +0300

    add appTests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,10 +1,15 @@
 package hexlet.code;
 
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlsRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+
+import java.sql.SQLTimeoutException;
+import java.sql.Timestamp;
 
 class AppTest {
 
@@ -16,9 +21,35 @@ class AppTest {
     }
 
     @Test
-    void testMainPage() throws Exception {
+    void testMainPage() {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.post("/urls", "url=\"https://ru.hexlet.io/projects/72/members/39558?step=5\"");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    void testShowUrl() throws SQLTimeoutException {
+        var url = new Url("https://ru.hexlet.io", new Timestamp(System.currentTimeMillis()));
+        UrlsRepository.save(url);
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/" + url.getId());
             assertThat(response.code()).isEqualTo(200);
         });
     }
